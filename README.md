b4377bba4c5d4fb342f920932e88d7aa6914e9ce
Remy Wang <remywang@cs.ucla.edu>
2025-06-16 18:16:32 -0700

    Improve README

---
+++ README.md
@@ -0,0 +1,3 @@
+See [imdb/README](./imdb/README) to generate data, then run the following command under project root:
+
+cargo run --release
--- imdb/csv2parquet.sql
+++ imdb/csv2parquet.sql
@@ -168,3 +168,47 @@ CREATE TABLE title (
     series_years character varying(49),
     md5sum character varying(32)
 );
+
+copy aka_name from 'aka_name.csv' escape '\';
+copy aka_title from 'aka_title.csv' escape '\';
+copy cast_info from 'cast_info.csv' escape '\';
+copy char_name from 'char_name.csv' escape '\';
+copy comp_cast_type from 'comp_cast_type.csv' escape '\';
+copy company_name from 'company_name.csv' escape '\';
+copy company_type from 'company_type.csv' escape '\';
+copy complete_cast from 'complete_cast.csv' escape '\';
+copy info_type from 'info_type.csv' escape '\';
+copy keyword from 'keyword.csv' escape '\';
+copy kind_type from 'kind_type.csv' escape '\';
+copy link_type from 'link_type.csv' escape '\';
+copy movie_companies from 'movie_companies.csv' escape '\';
+copy movie_info from 'movie_info.csv' escape '\';
+copy movie_info_idx from 'movie_info_idx.csv' escape '\';
+copy movie_keyword from 'movie_keyword.csv' escape '\';
+copy movie_link from 'movie_link.csv' escape '\';
+copy name from 'name.csv' escape '\';
+copy person_info from 'person_info.csv' escape '\';
+copy role_type from 'role_type.csv' escape '\';
+copy title from 'title.csv' escape '\';
+
+copy aka_name to 'aka_name.parquet' (format parquet);
+copy aka_title to 'aka_title.parquet' (format parquet);
+copy cast_info to 'cast_info.parquet' (format parquet);
+copy char_name to 'char_name.parquet' (format parquet);
+copy comp_cast_type to 'comp_cast_type.parquet' (format parquet);
+copy company_name to 'company_name.parquet' (format parquet);
+copy company_type to 'company_type.parquet' (format parquet);
+copy complete_cast to 'complete_cast.parquet' (format parquet);
+copy info_type to 'info_type.parquet' (format parquet);
+copy keyword to 'keyword.parquet' (format parquet);
+copy kind_type to 'kind_type.parquet' (format parquet);
+copy link_type to 'link_type.parquet' (format parquet);
+copy movie_companies to 'movie_companies.parquet' (format parquet);
+copy movie_info to 'movie_info.parquet' (format parquet);
+copy movie_info_idx to 'movie_info_idx.parquet' (format parquet);
+copy movie_keyword to 'movie_keyword.parquet' (format parquet);
+copy movie_link to 'movie_link.parquet' (format parquet);
+copy name to 'name.parquet' (format parquet);
+copy person_info to 'person_info.parquet' (format parquet);
+copy role_type to 'role_type.parquet' (format parquet);
+copy title to 'title.parquet' (format parquet);
