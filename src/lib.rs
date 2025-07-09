9a004b36e099281512a032058fd1e5e870ea1dc1
xxks-kkk <ferrishu3886@gmail.com>
2025-07-08 19:11:45 -0700

    Add JOB 5c
    ** Why is this change required? **
    
    [src/o5c.rs:109:5] start.elapsed() = 225.95672ms
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,7 @@ pub mod o4b;
 pub mod o4c;
 pub mod o5a;
 pub mod o5b;
+pub mod o5c;
 pub mod o6f;
 pub mod o7c;
 pub mod o8c;
---
+++ src/o5c.rs
@@ -0,0 +1,144 @@
+use crate::data::ImdbData;
+use ahash::HashSet;
+use polars::prelude::*;
+use std::time::Instant;
+
+pub fn q5c(db: &ImdbData) -> Result<Option<&str>, PolarsError> {
+    let ct = &db.ct;
+    let it = &db.it;
+    let mc = &db.mc;
+    let mi = &db.mi;
+    let t = &db.t;
+
+    let start = Instant::now();
+
+    let ct_s: HashSet<i32> = ct
+        .column("kind")?
+        .str()?
+        .into_iter()
+        .zip(ct.column("id")?.i32()?.into_iter())
+        .filter_map(|(kind, id)| {
+            if let (Some(kind), Some(id)) = (kind, id) {
+                if kind == "production companies" {
+                    Some(id)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mc_s: HashSet<i32> = mc
+        .column("note")?
+        .str()?
+        .into_iter()
+        .zip(mc.column("movie_id")?.i32()?.into_iter())
+        .zip(mc.column("company_type_id")?.i32()?.into_iter())
+        .filter_map(|((note, movie_id), company_type_id)| {
+            if let (Some(note), Some(movie_id), Some(company_type_id)) =
+                (note, movie_id, company_type_id)
+            {
+                if note.contains("(USA)")
+                    && !note.contains("(TV)")
+                    && ct_s.contains(&company_type_id)
+                {
+                    Some(movie_id)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mi_s: HashSet<i32> = mi
+        .column("info")?
+        .str()?
+        .into_iter()
+        .zip(mi.column("movie_id")?.i32()?.into_iter())
+        .filter_map(|(info, movie_id)| {
+            if let (Some(info), Some(movie_id)) = (info, movie_id) {
+                if matches!(
+                    info,
+                    "Sweden"
+                        | "Norway"
+                        | "Germany"
+                        | "Denmark"
+                        | "Swedish"
+                        | "Denish"
+                        | "Norwegian"
+                        | "German"
+                        | "USA"
+                        | "American"
+                ) {
+                    Some(movie_id)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut res: Option<&str> = None;
+
+    for ((id, production_year), title) in t
+        .column("id")?
+        .i32()?
+        .into_iter()
+        .zip(t.column("production_year")?.i32()?.into_iter())
+        .zip(t.column("title")?.str()?.into_iter())
+    {
+        if let (Some(id), Some(production_year), Some(title)) = (id, production_year, title) {
+            if production_year > 1990 && mc_s.contains(&id) && mi_s.contains(&id) {
+                if let Some(old_title) = res.as_mut() {
+                    if title < *old_title {
+                        *old_title = title;
+                    }
+                } else {
+                    res = Some(title);
+                }
+            }
+        }
+    }
+
+    dbg!(start.elapsed());
+
+    Ok(res)
+}
+
+// -- JOB Query 5c
+// SELECT MIN(t.title) AS american_movie
+// FROM company_type AS ct,
+// info_type AS it,
+// movie_companies AS mc,
+// movie_info AS mi,
+// title AS t
+// WHERE ct.kind = 'production companies'
+// AND mc.note not like '%(TV)%'
+// and mc.note like '%(USA)%'
+// AND mi.info IN
+// ('Sweden', 'Norway', 'Germany', 'Denmark', 'Swedish', 'Denish', 'Norwegian', 'German', 'USA', 'American')
+// AND t.production_year > 1990
+// AND t.id = mi.movie_id
+// AND t.id = mc.movie_id
+// AND mc.movie_id = mi.movie_id
+// AND ct.id = mc.company_type_id
+// AND it.id = mi.info_type_id;
+#[cfg(test)]
+mod test_5c {
+    use super::*;
+    use crate::data::ImdbData;
+
+    #[test]
+    fn test_q5c() -> Result<(), PolarsError> {
+        let db = ImdbData::new();
+        let res = q5c(&db)?;
+        assert_eq!(res, Some("11,830,420"));
+        Ok(())
+    }
+}
