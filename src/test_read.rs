a9d08e0213c866fa1ee1a413ac7d6a8e0fc017da
xxks-kkk <ferrishu3886@gmail.com>
2025-07-19 13:15:27 -0700

    Refactor test_read
    ** Why is this change required? **
    
    We would like to eliminate the possibility that using Polars API
    in-memory (test2) could be faster than our vanilla implementation
    (test1). The result is test2 slower than test1.
    
    Polars is configured to use 1 threads.
    Elapsed: 84.10ms
    Not push down: {414711: "Downey Jr., Robert", 414712: "Downey Sr., Robert"}
    Polars is configured to use 1 threads.
    Elapsed: 157.66ms
    Push down: {414711: "Downey Jr., Robert", 414712: "Downey Sr., Robert"}
    Polars is configured to use 1 threads.
    Elapsed: 254.05ms
    Push down2: {414711: "Downey Jr., Robert", 414712: "Downey Sr., Robert"}
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

--- src/test_read.rs
+++ src/test_read.rs
@@ -1,18 +1,11 @@
 use crate::data;
 use ahash::HashMap;
-use polars::prelude::{col, lit, IntoLazy, LazyFrame};
+use polars::prelude::{col, lit, IntoLazy, LazyFrame, ScanArgsParquet};
 use std::env;
 use std::time::Instant;
 
 /// Compare the performance of push down selection predicate when configure
 /// Polars into single-thread.
-/// Polars is configured to use 1 threads.
-//  Elapsed: 147.95ms
-//  Not push down: {414712: "Downey Sr., Robert", 414711: "Downey Jr., Robert"}
-//  Elapsed: 172.04ms
-//  Push down: {414711: "Downey Jr., Robert", 414712: "Downey Sr., Robert"}
-//  Elapsed: 281.54ms
-//  Push down2: {414711: "Downey Jr., Robert", 414712: "Downey Sr., Robert"}
 pub fn test_read() -> Result<(), Box<dyn std::error::Error>> {
     unsafe {
         env::set_var("POLARS_MAX_THREADS", "1");
@@ -20,9 +13,18 @@ pub fn test_read() -> Result<(), Box<dyn std::error::Error>> {
     let thread_count = &polars_core::POOL.current_num_threads();
     println!("Polars is configured to use {} threads.", thread_count);
 
-    let db = data::ImdbData::new();
+    test1()?;
+    test2()?;
+    test3()?;
 
+    Ok(())
+}
+
+// Test 1: Vanilla filter in-memory
+pub fn test1() -> Result<(), Box<dyn std::error::Error>> {
+    let db = data::ImdbData::new();
     let n = &db.n;
+
     let now = Instant::now();
     let n_m: HashMap<i32, &str> = n
         .column("id")?
@@ -30,12 +32,9 @@ pub fn test_read() -> Result<(), Box<dyn std::error::Error>> {
         .into_iter()
         .zip(n.column("name")?.str()?)
         .filter_map(|(id, name)| {
-            if let (Some(id), Some(name)) = (id, name) {
-                if name.contains("Downey") && name.contains("Robert") {
-                    Some((id, name))
-                } else {
-                    None
-                }
+            let (id, name) = (id?, name?);
+            if name.contains("Downey") && name.contains("Robert") {
+                Some((id, name))
             } else {
                 None
             }
@@ -43,17 +42,52 @@ pub fn test_read() -> Result<(), Box<dyn std::error::Error>> {
         .collect();
     println!("Elapsed: {:.2?}", now.elapsed());
     println!("Not push down: {:?}", n_m);
+    Ok(())
+}
+
+// Test 2: Using Polars API to filter in-memory
+pub fn test2() -> Result<(), Box<dyn std::error::Error>> {
+    let db = data::ImdbData::new();
+    let thread_count = &polars_core::POOL.current_num_threads();
+    println!("Polars is configured to use {} threads.", thread_count);
 
     let now2 = Instant::now();
-    let filtered_df = n
-        .clone()
-        .lazy()
+    let filtered_df =
+        db.n.lazy()
+            .filter(
+                col("name")
+                    .str()
+                    .contains(lit("Downey"), true)
+                    .and(col("name").str().contains(lit("Robert"), true)),
+            )
+            .select([col("id"), col("name")]) // Only select needed columns
+            .collect()?;
+
+    let n_m: HashMap<i32, &str> = filtered_df
+        .column("id")?
+        .i32()?
+        .into_iter()
+        .zip(filtered_df.column("name")?.str()?)
+        .filter_map(|(id, name)| Some((id?, name?)))
+        .collect();
+    println!("Elapsed: {:.2?}", now2.elapsed());
+    println!("Push down: {:?}", n_m);
+    Ok(())
+}
+
+// Test 3: filter data during read parquet
+pub fn test3() -> Result<(), Box<dyn std::error::Error>> {
+    let thread_count = &polars_core::POOL.current_num_threads();
+    println!("Polars is configured to use {} threads.", thread_count);
+    let now3 = Instant::now();
+    let filtered_df = LazyFrame::scan_parquet("imdb/name.parquet", ScanArgsParquet::default())?
         .filter(
             col("name")
                 .str()
                 .contains(lit("Downey"), true)
                 .and(col("name").str().contains(lit("Robert"), true)),
         )
+        .select([col("id"), col("name")]) // Only read needed columns from disk
         .collect()?;
 
     let n_m: HashMap<i32, &str> = filtered_df
@@ -61,45 +95,9 @@ pub fn test_read() -> Result<(), Box<dyn std::error::Error>> {
         .i32()?
         .into_iter()
         .zip(filtered_df.column("name")?.str()?)
-        .filter_map(|(id, name)| {
-            if let (Some(id), Some(name)) = (id, name) {
-                Some((id, name))
-            } else {
-                None
-            }
-        })
-        .collect();
-    println!("Elapsed: {:.2?}", now2.elapsed());
-    println!("Push down: {:?}", n_m);
-
-    let predicate = col("name")
-        .str()
-        .contains_literal(lit("Downey"))
-        .and(col("name").str().contains_literal(lit("Robert")));
-
-    let now3 = Instant::now();
-    let filtered_df = LazyFrame::scan_parquet(
-        "imdb/name.parquet", // File path
-        Default::default(),  // Scan options
-    )?
-    .filter(predicate)
-    .collect()?;
-
-    let n_m: HashMap<i32, &str> = filtered_df
-        .column("id")?
-        .i32()?
-        .into_iter()
-        .zip(filtered_df.column("name")?.str()?)
-        .filter_map(|(id, name)| {
-            if let (Some(id), Some(name)) = (id, name) {
-                Some((id, name))
-            } else {
-                None
-            }
-        })
+        .filter_map(|(id, name)| Some((id?, name?)))
         .collect();
     println!("Elapsed: {:.2?}", now3.elapsed());
     println!("Push down2: {:?}", n_m);
-
     Ok(())
 }
