b222baae21b92d936837edcf878393bfb97957f9
xxks-kkk <ferrishu3886@gmail.com>
2025-07-21 22:36:38 -0700

    Optimize 6e
    ** Why is this change required? **
    
    6e,0.2655542
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

--- src/o6e.rs
+++ src/o6e.rs
@@ -1,5 +1,5 @@
 use crate::data::ImdbData;
-use ahash::HashMap;
+use ahash::{HashMap, HashSet};
 use polars::prelude::*;
 use std::time::Instant;
 
@@ -12,119 +12,78 @@ pub fn q6e(db: &ImdbData) -> Result<Option<(&str, &str, &str)>, PolarsError> {
 
     let start = Instant::now();
 
-    let n_m: HashMap<i32, Vec<&str>> = n
+    let n_m: HashMap<i32, &str> = n
         .column("id")?
         .i32()?
-        .into_iter()
-        .zip(n.column("name")?.str()?)
+        .into_no_null_iter()
+        .zip(n.column("name")?.str()?.into_no_null_iter())
         .filter_map(|(id, name)| {
-            if let (Some(id), Some(name)) = (id, name) {
-                if name.contains("Downey") && name.contains("Robert") {
-                    Some((id, name))
-                } else {
-                    None
-                }
+            if name.contains("Downey") && name.contains("Robert") {
+                Some((id, name))
             } else {
                 None
             }
         })
-        .fold(HashMap::default(), |mut acc, (id, name)| {
-            acc.entry(id).or_default().push(name);
-            acc
-        });
+        .collect();
 
-    let mut k_m: HashMap<i32, Vec<&str>> = HashMap::default();
+    let k_id = k
+        .column("keyword")?
+        .str()?
+        .into_no_null_iter()
+        .zip(k.column("id")?.i32()?.into_no_null_iter())
+        .find(|(keyword, _)| *keyword == "marvel-cinematic-universe")
+        .map(|(_, id)| id)
+        .unwrap();
 
-    for (id, keyword) in k
-        .column("id")?
-        .i32()?
-        .into_iter()
-        .zip(k.column("keyword")?.str()?.into_iter())
-    {
-        if let (Some(id), Some(keyword)) = (id, keyword) {
-            if keyword == "marvel-cinematic-universe" {
-                k_m.entry(id).or_default().push(keyword);
-            }
-        }
-    }
-
-    let mut mk_m: HashMap<i32, Vec<i32>> = HashMap::default();
-
-    for (movie_id, keyword_id) in mk
+    let mk_s: HashSet<i32> = mk
         .column("movie_id")?
         .i32()?
-        .into_iter()
-        .zip(mk.column("keyword_id")?.i32()?.into_iter())
-    {
-        if let (Some(movie_id), Some(keyword_id)) = (movie_id, keyword_id) {
-            if k_m.contains_key(&keyword_id) {
-                mk_m.entry(movie_id).or_default().push(keyword_id);
+        .into_no_null_iter()
+        .zip(mk.column("keyword_id")?.i32()?.into_no_null_iter())
+        .filter_map(|(movie_id, keyword_id)| {
+            if k_id == keyword_id {
+                Some(movie_id)
+            } else {
+                None
             }
-        }
-    }
+        })
+        .collect();
 
-    let mut t_m: HashMap<i32, Vec<&str>> = HashMap::default();
-
-    for ((id, title), production_year) in t
+    let t_m: HashMap<i32, &str> = t
         .column("id")?
         .i32()?
-        .into_iter()
-        .zip(t.column("title")?.str()?.into_iter())
-        .zip(t.column("production_year")?.i32()?.into_iter())
-    {
-        if let (Some(id), Some(title), Some(production_year)) = (id, title, production_year) {
-            if mk_m.contains_key(&id) && production_year > 2000 {
-                t_m.entry(id).or_default().push(title);
-            }
-        }
-    }
+        .into_no_null_iter()
+        .zip(t.column("title")?.str()?.into_no_null_iter())
+        .zip(t.column("production_year")?.i32()?)
+        .filter_map(|((id, title), production_year)| {
+            production_year
+                .filter(|&year| year > 2000 && mk_s.contains(&id))
+                .map(|_| (id, title))
+        })
+        .collect();
 
-    let mut res: Option<(&str, &str, &str)> = None;
+    let mut res: Option<(&str, &str)> = None;
 
     for (pid, mid) in ci
         .column("person_id")?
         .i32()?
-        .into_iter()
-        .zip(ci.column("movie_id")?.i32()?.into_iter())
+        .into_no_null_iter()
+        .zip(ci.column("movie_id")?.i32()?.into_no_null_iter())
     {
-        if let (Some(pid), Some(mid)) = (pid, mid) {
-            if let Some(titles) = t_m.get(&mid) {
-                if let Some(kids) = mk_m.get(&mid) {
-                    if let Some(names) = n_m.get(&pid) {
-                        for kid in kids {
-                            if let Some(keywords) = k_m.get(kid) {
-                                for title in titles {
-                                    for keyword in keywords {
-                                        for name in names {
-                                            if let Some((old_keyword, old_name, old_title)) =
-                                                res.as_mut()
-                                            {
-                                                if name < old_name {
-                                                    *old_name = name;
-                                                }
-                                                if keyword < old_keyword {
-                                                    *old_keyword = keyword;
-                                                }
-                                                if title < old_title {
-                                                    *old_title = title;
-                                                }
-                                            } else {
-                                                res = Some((keyword, name, title));
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        if mk_s.contains(&mid)
+            && let Some(name) = n_m.get(&pid)
+            && let Some(title) = t_m.get(&mid)
+        {
+            res = match res {
+                Some((old_name, old_title)) => Some((name.min(&old_name), title.min(&old_title))),
+                None => Some((name, title)),
+            };
         }
     }
 
     println!("6e,{:}", start.elapsed().as_secs_f32());
 
-    Ok(res)
+    Ok(res.map(|(name, title)| ("marvel-cinematic-universe", name, title)))
 }
 
 // -- JOB Query 6e
