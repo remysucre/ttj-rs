802bf04f3b51cff2a48742a021e899dc844002d3
xxks-kkk <ferrishu3886@gmail.com>
2025-07-23 17:28:03 -0700

    Optimize o8c
    ** Why is this change required? **
    
    Optimize o8c based on our Polars API. Also, fixed the test case.
    
    8c,0.37011886
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

--- src/o8c.rs
+++ src/o8c.rs
@@ -13,89 +13,49 @@ pub fn q8c(db: &ImdbData) -> Result<Option<(&str, &str)>, PolarsError> {
     let mc = &db.mc;
     let cn = &db.cn;
 
-    let mut an_m: HashMap<i32, Vec<&str>> = HashMap::default();
-
-    for (id, name) in an
+    let an_m: HashMap<i32, Vec<&str>> = an
         .column("person_id")?
         .i32()?
-        .into_iter()
-        .zip(an.column("name")?.str()?.into_iter())
-    {
-        if let (Some(id), Some(name)) = (id, name) {
-            an_m.entry(id).or_default().push(name);
-        }
-    }
-
-    let t_m: HashMap<i32, Vec<&str>> = t
-        .column("id")?
-        .i32()?
-        .into_iter()
-        .zip(t.column("title")?.str()?)
-        .fold(HashMap::default(), |mut acc, (id, title)| {
-            if let (Some(id), Some(title)) = (id, title) {
-                acc.entry(id).or_default().push(title);
-                acc
-            } else {
-                acc
-            }
+        .into_no_null_iter()
+        .zip(an.column("name")?.str()?.into_no_null_iter())
+        .fold(HashMap::default(), |mut acc, (person_id, name)| {
+            acc.entry(person_id).or_default().push(name);
+            acc
         });
 
-    let n_s: HashSet<i32> = n.column("id")?.i32()?.into_iter().flatten().collect();
+    let t_m: HashMap<i32, &str> = t
+        .column("id")?
+        .i32()?
+        .into_no_null_iter()
+        .zip(t.column("title")?.str()?.into_no_null_iter())
+        .collect();
+
+    let n_s: HashSet<i32> = n.column("id")?.i32()?.into_no_null_iter().collect();
 
     let start = Instant::now();
 
     let rt_s: HashSet<i32> = rt
         .column("role")?
         .str()?
-        .into_iter()
-        .zip(rt.column("id")?.i32()?)
-        .filter_map(|(role, id)| {
-            if let (Some(role), Some(id)) = (role, id) {
-                if role == "writer" {
-                    Some(id)
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
+        .into_no_null_iter()
+        .zip(rt.column("id")?.i32()?.into_no_null_iter())
+        .filter_map(|(role, id)| (role == "writer").then_some(id))
         .collect();
 
     let cn_s: HashSet<i32> = cn
         .column("country_code")?
         .str()?
         .into_iter()
-        .zip(cn.column("id")?.i32()?)
-        .filter_map(|(country_code, id)| {
-            if let (Some(country_code), Some(id)) = (country_code, id) {
-                if country_code == "[us]" {
-                    Some(id)
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
+        .zip(cn.column("id")?.i32()?.into_no_null_iter())
+        .filter_map(|(country_code, id)| country_code.filter(|&code| code == "[us]").map(|_| id))
         .collect();
 
     let mc_s: HashSet<i32> = mc
         .column("movie_id")?
         .i32()?
-        .into_iter()
-        .zip(mc.column("company_id")?.i32()?)
-        .filter_map(|(movie_id, company_id)| {
-            if let (Some(movie_id), Some(company_id)) = (movie_id, company_id) {
-                if cn_s.contains(&company_id) {
-                    Some(movie_id)
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
+        .into_no_null_iter()
+        .zip(mc.column("company_id")?.i32()?.into_no_null_iter())
+        .filter_map(|(movie_id, company_id)| cn_s.contains(&company_id).then_some(movie_id))
         .collect();
 
     let mut res: Option<(&str, &str)> = None;
@@ -103,33 +63,27 @@ pub fn q8c(db: &ImdbData) -> Result<Option<(&str, &str)>, PolarsError> {
     for ((movie_id, person_id), role_id) in ci
         .column("movie_id")?
         .i32()?
-        .into_iter()
-        .zip(ci.column("person_id")?.i32()?.into_iter())
-        .zip(ci.column("role_id")?.i32()?.into_iter())
+        .into_no_null_iter()
+        .zip(ci.column("person_id")?.i32()?.into_no_null_iter())
+        .zip(ci.column("role_id")?.i32()?.into_no_null_iter())
     {
-        if let (Some(movie_id), Some(person_id), Some(role_id)) = (movie_id, person_id, role_id) {
-            if rt_s.contains(&role_id) && n_s.contains(&person_id) && mc_s.contains(&movie_id) {
-                if let (Some(name), Some(title)) = (an_m.get(&person_id), t_m.get(&movie_id)) {
-                    for name in name {
-                        for title in title {
-                            if let Some((old_name, old_title)) = res.as_mut() {
-                                if *old_title > *title {
-                                    *old_title = title;
-                                }
-                                if *old_name > *name {
-                                    *old_name = name;
-                                }
-                            } else {
-                                res = Some((name, title));
-                            }
-                        }
-                    }
-                }
-            }
+        if rt_s.contains(&role_id)
+            && n_s.contains(&person_id)
+            && mc_s.contains(&movie_id)
+            && let Some(name) = an_m.get(&person_id)
+            && let Some(title) = t_m.get(&movie_id)
+        {
+            res = match res {
+                Some((old_name, old_title)) => Some((
+                    name.iter().min().unwrap().min(&old_name),
+                    title.min(&old_title),
+                )),
+                None => Some((name.iter().min().unwrap(), title)),
+            };
         }
     }
 
-    println!("8d,{:}", start.elapsed().as_secs_f32());
+    println!("8c,{:}", start.elapsed().as_secs_f32());
 
     Ok(res)
 }
@@ -162,10 +116,7 @@ mod test_8c {
     fn test_q8c() -> Result<(), PolarsError> {
         let db = ImdbData::new();
         let res = q8c(&db)?;
-        assert_eq!(
-            res,
-            Some(("\"Jenny from the Block\"", "#1 Cheerleader Camp"))
-        );
+        assert_eq!(res, Some(("\"A.J.\"", "#1 Cheerleader Camp")));
         Ok(())
     }
 }
