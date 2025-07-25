386f1b9b335f0911a2cb0071eb37fe5e5e3710ae
xxks-kkk <ferrishu3886@gmail.com>
2025-07-24 19:59:24 -0700

    Optimize 20a
    ** Why is this change required? **
    
    20a,0.18454382
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

--- src/o20a.rs
+++ src/o20a.rs
@@ -1,6 +1,7 @@
-use crate::data::ImdbData;
+use crate::data::Data;
 use ahash::HashMap;
 use ahash::HashSet;
+use memchr::memmem;
 use polars::prelude::*;
 use std::time::Instant;
 
@@ -14,64 +15,57 @@ use std::time::Instant;
 // ||imdb.q20a_comp_cast_type2(status_id)
 // |imdb_int.movie_keyword(movie_id,keyword_id)
 // ||imdb.q20a_keyword(keyword_id)
-pub fn q20a(db: &ImdbData) -> Result<Option<&str>, PolarsError> {
+pub fn q20a(db: &Data) -> Result<Option<&str>, PolarsError> {
     let ci = &db.ci;
     let chn = &db.chn;
     let t = &db.t;
     let kt = &db.kt;
     // let n = &db.n;
     let cc = &db.cc;
-    let cct1: &DataFrame = &db.cct;
-    let cct2: &DataFrame = &db.cct;
+    let cct = &db.cct;
     let mk = &db.mk;
     let k = &db.k;
 
+    let complete = memmem::Finder::new("complete");
+    let iron = memmem::Finder::new(b"Iron");
+    let man = memmem::Finder::new(b"Man");
+    let sherlock = memmem::Finder::new(b"Sherlock");
+    let tony = memmem::Finder::new(b"Tony");
+    let stark = memmem::Finder::new(b"Stark");
+
     let start = Instant::now();
 
-    let cct1_id = cct1
-        .column("id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(cct1.column("kind")?.str()?.into_no_null_iter())
-        .find(|(_, kind)| *kind == "cast")
-        .map(|(id, _)| id)
-        .unwrap();
+    let mut cct1_id: i32 = 0;
+    let mut cct2_s = HashSet::default();
 
-    let cct2_s: HashSet<i32> = cct2
-        .column("id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(cct2.column("kind")?.str()?.into_no_null_iter())
-        .filter_map(|(id, kind)| kind.contains("complete").then_some(id))
+    for (id, kind) in cct.id.iter().zip(cct.kind.iter()) {
+        if kind == "cast" {
+            cct1_id = *id;
+        } else if complete.find(kind.as_bytes()).is_some() {
+            cct2_s.insert(*id);
+        }
+    }
+
+    let cc_s: HashSet<i32> = cc
+        .subject_id
+        .iter()
+        .zip(cc.status_id.iter())
+        .zip(cc.movie_id.iter())
+        .filter_map(|((subject_id, status_id), movie_id)| {
+            (*subject_id == cct1_id && cct2_s.contains(status_id)).then_some(*movie_id)
+        })
         .collect();
 
-    let cc_s = cc
-        .column("subject_id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(cc.column("status_id")?.i32()?.into_no_null_iter())
-        .zip(cc.column("movie_id")?.i32()?.into_no_null_iter())
-        .filter_map(|((subject_id, status_id), movie_id)| {
-            (cct1_id == subject_id && cct2_s.contains(&status_id)).then_some(movie_id)
-        })
-        .collect::<HashSet<_>>();
-
     let chn_s = chn
-        .column("name")?
-        .str()?
-        .into_no_null_iter()
-        .zip(chn.column("id")?.i32()?.into_no_null_iter())
+        .name
+        .iter()
+        .zip(chn.id.iter())
         .filter_map(|(name, id)| {
-            if name.contains("Sherlock") {
-                return None;
-            }
-            if (name.contains("Tony") && name.contains("Stark"))
-                || (name.contains("Iron") && name.contains("Man"))
-            {
-                Some(id)
-            } else {
-                None
-            }
+            (sherlock.find(name.as_bytes()).is_none()
+                && ((iron.find(name.as_bytes()).is_some() && man.find(name.as_bytes()).is_some())
+                    || (tony.find(name.as_bytes()).is_some()
+                        && stark.find(name.as_bytes()).is_some())))
+            .then_some(id)
         })
         .collect::<HashSet<_>>();
 
@@ -88,41 +82,31 @@ pub fn q20a(db: &ImdbData) -> Result<Option<&str>, PolarsError> {
     .into_iter()
     .collect();
 
-    let k_s: HashSet<i32> = k
-        .column("id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(k.column("keyword")?.str()?.into_no_null_iter())
-        .filter_map(|(id, keyword)| target_keywords.contains(keyword).then_some(id))
-        .collect();
+    let k_s: HashSet<i32> =
+        k.id.iter()
+            .zip(k.keyword.iter())
+            .filter_map(|(id, keyword)| target_keywords.contains(keyword.as_str()).then_some(*id))
+            .collect();
 
     let kt_id = kt
-        .column("kind")?
-        .str()?
-        .into_no_null_iter()
-        .zip(kt.column("id")?.i32()?.into_no_null_iter())
+        .kind
+        .iter()
+        .zip(kt.id.iter())
         .find(|(kind, _)| *kind == "movie")
         .map(|(_, id)| id)
         .unwrap();
 
-    let t_m: HashMap<i32, &str> = t
-        .column("id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(t.column("title")?.str()?.into_no_null_iter())
-        .zip(t.column("production_year")?.i32()?)
-        .zip(t.column("kind_id")?.i32()?.into_no_null_iter())
-        .filter_map(|(((id, title), production_year), kind_id)| {
-            if kt_id != kind_id || !cc_s.contains(&id) {
-                return None;
-            }
-            if let Some(year) = production_year {
-                (year > 1950).then_some((id, title))
-            } else {
-                None
-            }
-        })
-        .collect();
+    let t_m: HashMap<i32, &str> =
+        t.id.iter()
+            .zip(t.title.iter())
+            .zip(t.production_year.iter())
+            .zip(t.kind_id.iter())
+            .filter_map(|(((id, title), production_year), kind_id)| {
+                production_year
+                    .filter(|&year| year > 1950 && kt_id == kind_id && cc_s.contains(&id))
+                    .map(|_| (*id, title.as_str()))
+            })
+            .collect();
 
     // let n_s = n
     //     .column("id")?
@@ -132,24 +116,23 @@ pub fn q20a(db: &ImdbData) -> Result<Option<&str>, PolarsError> {
     //     .collect::<HashSet<_>>();
 
     let mk_s = mk
-        .column("keyword_id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(mk.column("movie_id")?.i32()?.into_no_null_iter())
+        .keyword_id
+        .iter()
+        .zip(mk.movie_id.iter())
         .filter_map(|(keyword_id, movie_id)| {
             (k_s.contains(&keyword_id) && t_m.contains_key(&movie_id)).then_some(movie_id)
         })
         .collect::<HashSet<_>>();
 
     let matching_movies: Vec<i32> = ci
-        .column("movie_id")?
-        .i32()?
-        .into_no_null_iter()
-        .zip(ci.column("person_role_id")?.i32()?.into_no_null_iter())
-        .filter(|(movie_id, person_role_id)| {
-            chn_s.contains(person_role_id) && mk_s.contains(movie_id)
+        .movie_id
+        .iter()
+        .zip(ci.person_role_id.iter())
+        .filter_map(|(movie_id, person_role_id)| {
+            person_role_id
+                .filter(|id| chn_s.contains(id) && mk_s.contains(movie_id))
+                .map(|_| *movie_id)
         })
-        .map(|(movie_id, _)| movie_id)
         .collect();
 
     let res = matching_movies
@@ -206,12 +189,13 @@ pub fn q20a(db: &ImdbData) -> Result<Option<&str>, PolarsError> {
 #[cfg(test)]
 mod test_20a {
     use super::*;
-    use crate::data::ImdbData;
+    use crate::data::{Data, ImdbData};
 
     #[test]
     fn test_q20a() -> Result<(), PolarsError> {
         let db = ImdbData::new();
-        let res = q20a(&db)?;
+        let data = Data::new(&db);
+        let res = q20a(&data)?;
         assert_eq!(res, Some("Disaster Movie"));
         Ok(())
     }
